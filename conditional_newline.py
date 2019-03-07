d7b32a35ac04b9d8e062800b8f0790c601879f8a
Colin Panisset <colin.panisset@cevo.com.au>
2019-03-07 14:09:05 +1100

    get the terminal size on he fly and use that to calculate a margin

--- conditional_newline.py
+++ conditional_newline.py
@@ -1,6 +1,7 @@
 from powerline_shell.utils import BasicSegment
 import unicodedata, re
 import os
+import fcntl, termios, struct
 
 control_char_re = re.compile('\\[.*?\\]')
 
@@ -11,6 +12,13 @@ class Segment(BasicSegment):
     def add_to_powerline(self):
         result = remove_control_chars(self.powerline.draw())
         width = len(result)
-        max_width = int(os.getenv("POWERLINE_MAX_WIDTH", 80))
+        margin = int(os.getenv("POWERLINE_MARGIN", 20))
+        try:
+            res = fcntl.ioctl(0, termios.TIOCGWINSZ, '        ')
+            cols = struct.unpack('HHHH', res)[1] - margin
+        except Exception as e:
+            print(f'Exception getting terminal size: {e}')
+        # an override, if I want to force it
+        max_width = int(os.getenv("POWERLINE_MAX_WIDTH", cols))
         if width > max_width:
             self.powerline.append("\n", self.powerline.theme.RESET, self.powerline.theme.RESET, separator="")
